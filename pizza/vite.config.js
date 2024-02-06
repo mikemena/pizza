8b0a331cdcb79fc570685cb698ab10aa42ca660c
tokyo <tokyolabs@protonmail.com>
2024-02-06 18:59:00 -0500

    wip

--- pizza/vite.config.js
+++ pizza/vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslintPlugin from 'vite-plugin-eslint';
 import prettierPlugin from 'vite-plugin-prettier';
+import open from 'open';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin(), prettierPlugin()],
+  server: {
+    configResolved(config) {
+      if (config.command === 'serve' && config.mode === 'development') {
+        setTimeout(() => {
+          open('http://localhost:3000', { app: { name: open.apps.chrome } });
+        }, 1000); // Adjust the delay as needed
+      }
+    },
+  },
 });
