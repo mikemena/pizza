055bcccaffb2a537af99a25e8a06e7a280f20e3c
tokyo <tokyolabs@protonmail.com>
2024-02-07 07:31:18 -0500

    wip

--- pizza/src/App.jsx
+++ pizza/src/App.jsx
@@ -1,22 +1,68 @@
-import "./App.css";
-// import data from '../public/data';
+/* eslint-disable react/prop-types */
+import "./app.css";
 
-function Pizza() {
+function Header() {
   return (
-    <>
-      <img src="/pizzas/focaccia.jpg" />
-      <h2>Focaccia</h2>
-      <p>Bread with italian olive oil and rosemary</p>
-    </>
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza
+        name="Focaccia"
+        ingredient="Bread with italian olive oil and rosemary"
+        photo="/pizzas/focaccia.jpg"
+        price={8}
+      />
+      <Pizza
+        name="Pizza Margherita"
+        ingredient="Tomato and mozarella"
+        photo="/pizzas/margherita.jpg"
+        price={10}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photo} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredient}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log("isOpen ->", isOpen);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We are currently open
+    </footer>
   );
 }
 
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-      <Pizza />
-    </>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
   );
 }
 
